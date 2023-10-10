8d99c03a23877f57a9c37bb7298b38104103d512
ihasina <hasinarasolofndraibe77@gmail.com>
2023-10-10 14:37:48 +0300

    add login signup and login feature

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,11 +1,48 @@
 const User = require('../entity/User.entity')
+const { hashPassword, comparePassword } = require('../lib/auth').auth;
 
 const createUser = async(req, res, next) => {
-  const user = new User(req.body)
-  const doc = await user.save()
-  res.json({ message: "Created" })
+  try {
+    const user = new User(req.body)
+    const passHashed = await hashPassword(req.body.password);
+    user.password = passHashed;
+
+    const doc = await user.save()
+    res.json(
+      { 
+        message: "Created",
+        data: doc
+      }
+    )
+  } catch(err) {
+    console.error(err.message);
+  }
+}
+
+const login = async(req, res, next) => {
+  try {
+    const userToFind = await User.findOne({ username: req.body.username });
+    if(!userToFind) {
+      return res.status(401).json({ message: "User does not exists" });
+    }
+
+    const isAuthenticate = comparePassword(req.body.password, userToFind.password);
+
+    if(isAuthenticate) {
+      return res.status(201).json({
+        user: userToFind
+      })
+    } else {
+      return res.status(401).json({
+        message: "username or password incorrect"
+      })
+    }
+  } catch(err) {
+    console.error(err.message);
+  }
 }
 
 module.exports = {
-  createUser
+  createUser,
+  login
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const userController = require('../controllers/user.controller');
 // import { userController } from '../controllers/user.controller';
 
+router.post('/login', userController.login);
 router.post("/create", userController.createUser)
 
 module.exports = router;
